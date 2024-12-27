e73938c0ac896f7f519bb51df6f176d0cb488106
snxq0 <isaievnikitaa@gmail.com>
2024-12-27 22:05:44 +0100

    Update cv_script.js

--- cv_script.js
+++ cv_script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById("redirectButton");
 
 button.addEventListener("click", () => {
-    window.location.href = "http://127.0.0.1:3000/web development/another.html"
+    window.location.href = "https://snxq0.github.io/Another/"
 })
